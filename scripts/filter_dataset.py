123e2392b4125b314c74bff06b9bc697d437bf71
nayeeee <nayeli.cayul@alumnos.uach.cl>
2025-03-14 22:17:39 -0300

    filter dataset

---
+++ scripts/filter_dataset.py
@@ -0,0 +1,17 @@
+# filter dataset
+
+# 1.- Extract N examples or all:
+# * where pf or frequency is not null 
+# * L points in each band 
+# * average magnitude in band G < M
+# * 
+
+
+
+
+
+
+
+
+
+
