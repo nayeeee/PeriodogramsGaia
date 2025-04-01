d0c140801e3e4a69374cb36d11637cbbbeb887ab
nayeeee <nayeli.cayul@alumnos.uach.cl>
2025-04-01 15:58:07 -0300

    Update dataset.py to enhance query logic for light curves. Added conditional handling for 'vari_eclipsing_binary' and improved output messages. Updated hardcoded password for Gaia login.

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -9,16 +9,23 @@ from matplotlib import pyplot as plt
 if __name__ == "__main__":
         # [("vari_eclipsing_binary", "frequency", 2184477), ("vari_rrlyrae", "pf", 271779)]
         for table, period_or_frequency, number_of_lc in [("vari_rrlyrae", "pf", 271779)]: 
-                Gaia.login(user='cnavarro', password='')
-                query = f"""
-                        SELECT * 
-                        FROM gaiadr3.{table}
-                        WHERE {period_or_frequency} IS NOT NULL
-                        """
+                Gaia.login(user='cnavarro', password='Nayeli20*')
+                if table == "vari_eclipsing_binary":
+                        query = f"""
+                                SELECT * 
+                                FROM gaiadr3.{table}
+                                WHERE {period_or_frequency} IS NOT NULL  
+                                """
+                else:
+                        query = f"""
+                                SELECT * 
+                                FROM gaiadr3.{table}
+                                WHERE {period_or_frequency} IS NOT NULL OR "p1_o" IS NOT NULL
+                                """
                 job = Gaia.launch_job_async(query, output_file=f"{table}.csv", output_format="csv", dump_to_file=True, verbose=True)
                 results = job.get_results()
                 print(f"Number of records in {table}:", len(results))
-                print(f"Number of lc with null {period_or_frequency} in {table}:", number_of_lc - len(results))
+                print(f"Number of lc with period null in {table}:", number_of_lc - len(results))
                 Gaia.logout()
         
 ## OUTPUT:
