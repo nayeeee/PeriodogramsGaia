f934c0c400257db62a9c14b683e606c0e381d27a
nayeeee <nayeli.cayul@alumnos.uach.cl>
2025-05-01 18:58:35 -0400

    Remove outdated files related to RR Lyrae periodograms and light curves, including JSON files and a text file tracking missing periodograms. Update dataset.py to enhance user input for Gaia login, improving security by removing hardcoded credentials.

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -4,12 +4,16 @@ import os
 import argparse
 from astroquery.gaia import Gaia 
 from matplotlib import pyplot as plt
+import getpass
         
 # main function
 if __name__ == "__main__":
         # [("vari_eclipsing_binary", "frequency", 2184477), ("vari_rrlyrae", "pf", 271779)]
         for table, period_or_frequency, number_of_lc in [("vari_rrlyrae", "pf", 271779)]: 
-                Gaia.login(user='cnavarro', password='Nayeli20*')
+                user = input("Username Gaia: ")
+                password = getpass.getpass("Password Gaia: ")
+                
+                Gaia.login(user=user, password=password)
                 if table == "vari_eclipsing_binary":
                         query = f"""
                                 SELECT * 
